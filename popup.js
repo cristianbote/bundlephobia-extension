eb4066be3baf952f0b404a69561d965f298003b7
Micah Miller Eshleman <micah@befunky.com>
2019-05-15 12:33:59 -0700

    Add support for scoped packages, e.g. @webcomponents/template

--- popup.js
+++ popup.js
@@ -1,19 +1,27 @@
-const getPackageName = url => url.split('/').filter(Boolean).slice(0, 4).slice(-1);
+const getPackageName = url => {
+    const parts = url.split('/').filter(Boolean);
+    const [org, name] = parts[1].endsWith('npmjs.com')
+        ? parts.slice(3, 5)
+        : parts.slice(2, 4);
+    return org.startsWith('@')
+        ? `${org}/${name}`
+        : name || org;
+};
 const getBundlephobiaUrl = name => `https://bundlephobia.com/result?p=${name}&src=bundlephobia-extension`;
 const getIframe = () => document.querySelector('iframe');
 
 chrome.tabs.query({
     active: true,
     currentWindow: true
-  }, ([currentTab]) => {
-       const url = currentTab.url;
-       const packageName = getPackageName(url);
-       const iframe = getIframe();
+}, ([currentTab]) => {
+    const url = currentTab.url;
+    const packageName = getPackageName(url);
+    const iframe = getIframe();
 
-       iframe.onload = () => {
-           document.body.className = "";
-           iframe.onload = null;
-       };
+    iframe.onload = () => {
+        document.body.className = "";
+        iframe.onload = null;
+    };
 
-       iframe.src = getBundlephobiaUrl(packageName);
-  });
+    iframe.src = getBundlephobiaUrl(packageName);
+});
